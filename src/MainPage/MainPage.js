f78c79673fcdccd6e0b7408442f2627682732f67
Wojtek M <wojciech.moczydlowski98@gmail.com>
2019-05-16 12:08:24 +0200

    visual part of searchInput

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,55 +1,64 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import '../styles/main.scss';
+import backgroundImg from "./img/sky.jpg";
 
-import {fetchTodayWeather} from '../actions/todayWeatherActions';
-import {fetchLongTimeWeather,displayLongTimeWeather} from '../actions/longTimeWeatherActions';
+import { fetchTodayWeather } from "../actions/todayWeatherActions";
+import {
+  fetchLongTimeWeather,
+  displayLongTimeWeather
+} from "../actions/longTimeWeatherActions";
 
-import TodayWeather from './components/TodayWeather';
-import OtherTownsWeather from './components/OtherTownsWeather';
-import SearchPanel from './components/SearchPanel';
-import NextDayWeather from './components/NextDayWeather';
-import LongTermWeather from './components/LongTermWeather';
-import Loading from './components/Loading';
+import TodayWeather from "./components/TodayWeather";
+import OtherTownsWeather from "./components/OtherTownsWeather";
+import SearchPanel from "./components/SearchPanel";
+import NextDayWeather from "./components/NextDayWeather";
+import LongTermWeather from "./components/LongTermWeather";
+import Loading from "./components/Loading";
+
+import AppBar from "@material-ui/core/AppBar";
 
 class MainPage extends Component {
-
-  componentDidMount(){
+  componentDidMount() {
     //fetchTodayWeather();
     this.props.dispatch(fetchTodayWeather());
   }
   render() {
-    const { todayWeather , loadingTodayWeather} = this.props;
-    const { longTimeWeather,loadingLongTimeWeather,errorLongTimeWeather,displayLongTimeWeather} = this.props;
+    const { todayWeather, loadingTodayWeather } = this.props;
+    const {
+      longTimeWeather,
+      loadingLongTimeWeather,
+      errorLongTimeWeather,
+      displayLongTimeWeather
+    } = this.props;
     console.log(longTimeWeather);
     return (
       <div className="main-page">
-        { loadingTodayWeather ? (<Loading/>) :( <TodayWeather todayWeather = {todayWeather}/> )} 
-        { loadingTodayWeather ? (<Loading/>) :( <NextDayWeather weather = {todayWeather}/>)} 
-        { loadingTodayWeather ? (<Loading/>) :( <NextDayWeather weather = {todayWeather}/>)} 
-        <OtherTownsWeather/>
-        <SearchPanel/>
-        { displayLongTimeWeather ? (<LongTermWeather/>) : <div/>}
+        <SearchPanel />
+        {loadingTodayWeather ? (
+          <Loading />
+        ) : (
+          <TodayWeather todayWeather={todayWeather} />
+        )}
+        {loadingTodayWeather ? (
+          <Loading />
+        ) : (
+          <NextDayWeather weather={todayWeather} />
+        )}
+        {loadingTodayWeather ? (
+          <Loading />
+        ) : (
+          <NextDayWeather weather={todayWeather} />
+        )}
+        {/* <OtherTownsWeather/> */}
+
+        {displayLongTimeWeather ? <LongTermWeather /> : <div />}
       </div>
     );
   }
 }
-  //  function fetchTodayWeather() {
-  //  return function(dispatch){
 
-  //   dispatch(todayWeatherRequest());
-  //   //const weather = await getTodayWeather();
-  //   const weather = {};
-  //  // dispatch(todayWeatherSuccess(weather));
-  //   console.log(weather);
-
-  //  }
-
-// }
-
-
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
   todayWeather: state.todayWeather.weather,
   loadingTodayWeather: state.todayWeather.loading,
   errorTodayWeather: state.todayWeather.error,
@@ -57,7 +66,10 @@ const mapStateToProps = state =>({
   longTimeWeather: state.longTimeWeather.weather,
   loadingLongTimeWeather: state.longTimeWeather.weather,
   errorLongTimeWeather: state.longTimeWeather.weather,
-  displayLongTimeWeather: state.longTimeWeather.display,
+  displayLongTimeWeather: state.longTimeWeather.display
 });
 
-export default connect(mapStateToProps,null)(MainPage);
+export default connect(
+  mapStateToProps,
+  null
+)(MainPage);
--- src/MainPage/components/SearchPanel.js
+++ src/MainPage/components/SearchPanel.js
@@ -1,28 +1,134 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import '../../styles/main.scss';
+import React, { Component } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import {weatherInputChange} from '../../actions/searchWeatherActions';
-import searchWeatherReducer from '../../reducers/searchWeatherReducer';
+import { weatherInputChange } from "../../actions/searchWeatherActions";
+import { searchWeatherReducer } from "../../reducers/searchWeatherReducer";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import Button from "@material-ui/core/Button";
 
 class SearchPanel extends Component {
-
-handleInput = (event) =>{
-  this.props.weatherInputChange(event.target.value);
- }
+  handleInput = event => {
+    this.props.weatherInputChange(event.target.value);
+  };
 
   render() {
+    const { classes } = this.props;
     return (
-      <div className="search-panel">
-          <input onChange = {this.handleInput} value = {this.props.cityName}/>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              Weather App
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search city"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Search
+            </Button>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-
-const mapStateToProps = state =>({
- cityName: state.searchWeatherReducer.cityName,
+const styles = theme => ({
+  root: {
+    width: "100%"
+  },
+  grow: {
+    flexGrow: 1
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
+  }
 });
 
-export default connect(null,{weatherInputChange})(SearchPanel);;
+const mapStateToProps = state => ({
+  cityName: state.searchWeather.cityName
+});
+
+SearchPanel.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default compose(
+  connect(
+    mapStateToProps,
+    { weatherInputChange }
+  ),
+  withStyles(styles)
+)(SearchPanel);
